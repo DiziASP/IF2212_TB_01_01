75adac8b2c32c6ac601a77e21a9c2bcae30e548d
Dizi <58800976+DiziASP@users.noreply.github.com>
2023-04-13 17:57:13 +0700

    feat: minor change

--- sims/src/main/java/if2212_tb_01_01/App.java
+++ sims/src/main/java/if2212_tb_01_01/App.java
@@ -3,12 +3,19 @@
  */
 package if2212_tb_01_01;
 
-public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+import javax.swing.*;
 
+public class App {
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        JFrame f = new JFrame();// creating instance of JFrame
+
+        JButton b = new JButton("click");// creating instance of JButton
+        b.setBounds(130, 100, 100, 40);// x axis, y axis, width, height
+
+        f.add(b);// adding button in JFrame
+
+        f.setSize(400, 500);// 400 width and 500 height
+        f.setLayout(null);// using no layout managers
+        f.setVisible(true);// making the frame visible
     }
 }
