0ca739a1455aa739ed51cb9c41812880b0f60066
MarioNicholas <18221061@std.stei.itb.ac.id>
2023-04-16 22:19:17 +0700

    feat. sim and inventory.java

---
+++ sims/src/main/java/if2212_tb_01_01/entities/Inventory.java
@@ -0,0 +1,62 @@
+import java.util.*;
+
+public class Inventory<T extends Objek> {
+    private Map<T, Integer> inventory;
+
+    public Inventory() {
+        inventory = new HashMap<T, Integer>();
+    }
+
+    public boolean isEmpty() {
+        return inventory.isEmpty();
+    }
+
+    public int jumlahItem(T item) {
+        if (inventory.containsKey(item)) {
+            return inventory.get(item);
+        }
+        else {
+            return 0;
+        }
+    }
+
+    public void addItem (T item, int quantity) {
+        if (inventory.containsKey(item)) {
+            inventory.put(item, inventory.get(item) + quantity);
+        }
+        else {
+            inventory.put(item, quantity);
+        }
+    }
+
+    public void removeItem (T item, int quantity) {
+        if (inventory.containsKey(item)) {
+            int remainder = inventory.get(item) - quantity;
+            if (remainder > 1) {
+                inventory.put(item, remainder);
+            }
+            else if (remainder == 0) {
+                inventory.remove(item);
+            }
+            else {
+                System.out.println("Insufficient Materials!");
+            }
+        }
+        else {
+            System.out.println("No item(s) to remove!");
+        }
+    }
+
+    public void displayInventory() {
+        System.out.println ("Inventory: ");
+        if (inventory.isEmpty()) {
+            System.out.println("Inventory is Empty");
+        }
+        else {
+            for (T item : inventory.keySet()) {
+                System.out.println(item.getName() + " : " + inventory.get(item));
+            }
+        }
+    }
+
+}   
--- sims/src/main/java/if2212_tb_01_01/entities/Sim.java
+++ sims/src/main/java/if2212_tb_01_01/entities/Sim.java
@@ -1,5 +1,4 @@
 package if2212_tb_01_01.entities;
-
 import java.util.*;
 
 /**
@@ -8,12 +7,6 @@ import java.util.*;
  */
 public class Sim {
     class Pekerjaan {
-    }
-
-    class Inventory {
-    }
-
-    class Kesejahteraan {
     }
 
     class Aksi {
@@ -29,11 +22,151 @@ public class Sim {
     /* Sim Attributes */
     private String namaLengkap;
     private Pekerjaan pekerjaan;
-    private Integer uang;
+    private int uang;
     private Inventory inventory;
     private String status;
-    private Kesejahteraan kesejahteraan;
+    private int kesehatan;
+    private int mood;
+    private int kekenyangan;
     private List<Aksi> aksi;
     private Point posisi;
     private Rumah rumah;
+
+    public Sim(String namaLengkap, Pekerjaan pekerjaan) {
+        this.namaLengkap = namaLengkap;
+        this.pekerjaan = pekerjaan;
+        this.uang = 100;
+        this.inventory = new Inventory();
+        this.kesehatan = 80;
+        this.mood = 80;
+        this.kekenyangan = 80;
+        this.status = "";
+    }
+
+    //Getter dan Setter
+    public String getNamaLengkap() {
+        return namaLengkap;
+    }
+
+    public void setNamaLengkap(String namaLengkap) {
+        this.namaLengkap = namaLengkap;
+    }
+
+    public Pekerjaan getPekerjaan() {
+        return pekerjaan;
+    }
+
+    public void setPekerjaan(Pekerjaan pekerjaan) {
+        this.pekerjaan = pekerjaan;
+    }
+
+    public int getUang() {
+        return uang;
+    }
+
+    public void setUang(int uang) {
+        this.uang = uang;
+    }
+
+    public Inventory getInventory() {
+        return inventory;
+    }
+
+    public void setInventory(Inventory inventory) {
+        this.inventory = inventory;
+    }
+
+    public int getKekenyangan() {
+        return kekenyangan;
+    }
+
+    public void setKekenyangan(int kekenyangan) {
+        this.kekenyangan = kekenyangan;
+    }
+
+    public int getMood() {
+        return mood;
+    }
+
+    public void setMood(int mood) {
+        this.mood = mood;
+    }
+
+    public int getKesehatan() {
+        return kesehatan;
+    }
+
+    public void setKesehatan(int kesehatan) {
+        this.kesehatan = kesehatan;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+
+    //Actions of Sim
+    public void addToInventory() {
+        //Please provide the solution below
+    }
+
+    public void removeFromInventory() {
+        //Please provide the solution below
+    }
+
+    public void kerja() {
+        //Please provide the solution below
+    }
+
+    public void olahraga() {
+        //Please provide the solution below
+    }
+
+    public void tidur() {
+        //Please provide the solution below
+    }
+
+    public void makan() {
+        //Please provide the solution below
+    }
+
+    public void memasak() {
+        //Please provide the solution below
+    }
+
+    public void berkunjung() {
+        //Please provide the solution below
+    }
+
+    public void buangAir() {
+        //Please provide the solution below
+    }
+
+    public void upgradeRumah() {
+        //Please provide the solution below
+    }
+
+    public void beliBarang() {
+        //Please provide the solution below
+    }
+
+    public void berpindahRuangan() {
+        //Please provide the solution below
+    }
+
+    public void melihatInventory() {
+        //Please provide the solution below
+    }
+
+    public void memasangBarang() {
+        //Please provide the solution below
+    }
+
+    public void melihatWaktu() {
+        //Please provide the solution below
+    }
 }
