0b7519d9656fb2c3d8cfcba2de246709de866d0f
nauravalda <16521058@mahasiswa.itb.ac.id>
2023-05-01 17:21:29 +0700

    worldClock iterasi detik kalo aksi aktif doang

--- app/src/main/java/if2212_tb_01_01/entities/world/World.java
+++ app/src/main/java/if2212_tb_01_01/entities/world/World.java
@@ -215,14 +215,12 @@ public class World {
     // }
 
     public boolean isIdle() {
-        boolean isIdle = false;
+        boolean isIdle = true;
         for (Sim sim : listSim) {
-            if (sim.getStatus().size() == 0) {
-                isIdle = true;
-            } else {
+            if (sim.getIsDoAksiAktif()) {
                 isIdle = false;
                 break;
-            }
+            } 
         }
         return isIdle;
     }
