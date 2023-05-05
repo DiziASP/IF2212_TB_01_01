e80e156235d5d9897fbbeed9b0d7f22db930b190
nauravalda <16521058@mahasiswa.itb.ac.id>
2023-05-05 21:06:28 +0700

    pecahin sim dan aksi

--- app/src/main/java/if2212_tb_01_01/entities/sim/Sim.java
+++ app/src/main/java/if2212_tb_01_01/entities/sim/Sim.java
@@ -34,105 +34,104 @@ public class Sim {
     int roomY = (screenHeight - tileSize * 11) / 2;
 
     //aksi
-    public static class 
-    Aksi{
-        private String nama;
-        private Sim sim;
-        private int detikTersisa;
-        private boolean isButuhObjek;
-        private int indexBeli;
-        private boolean isAksiPasif;
-        private Room roomUpgrade;
-        public Aksi(Sim sim, String nama, int jumlahSeconds){
-            this.sim = sim;
-            this.nama = nama;
-            this.detikTersisa = jumlahSeconds;
-            this.isAksiPasif =false;
-        }
-        public Aksi(String nama, boolean isButuhObjek){
-            this.nama = nama;
-            this.isButuhObjek = isButuhObjek;
-            this.detikTersisa = -1;
-            this.sim = null;
-        }
+    // public static class Aksi{
+    //     private String nama;
+    //     private Sim sim;
+    //     private int detikTersisa;
+    //     private boolean isButuhObjek;
+    //     private int indexBeli;
+    //     private boolean isAksiPasif;
+    //     private Room roomUpgrade;
+    //     public Aksi(Sim sim, String nama, int jumlahSeconds){
+    //         this.sim = sim;
+    //         this.nama = nama;
+    //         this.detikTersisa = jumlahSeconds;
+    //         this.isAksiPasif =false;
+    //     }
+    //     public Aksi(String nama, boolean isButuhObjek){
+    //         this.nama = nama;
+    //         this.isButuhObjek = isButuhObjek;
+    //         this.detikTersisa = -1;
+    //         this.sim = null;
+    //     }
 
-        public Aksi(Sim sim, String nama, int jumlahSeconds, boolean isAksiPasif){
-            this.sim = sim;
-            this.nama = nama;
-            this.detikTersisa = jumlahSeconds;
-            this.isAksiPasif = isAksiPasif;
-        }
+    //     public Aksi(Sim sim, String nama, int jumlahSeconds, boolean isAksiPasif){
+    //         this.sim = sim;
+    //         this.nama = nama;
+    //         this.detikTersisa = jumlahSeconds;
+    //         this.isAksiPasif = isAksiPasif;
+    //     }
 
-        public Aksi(Sim sim, String nama, int jumlahSeconds, boolean isAksiPasif, int indexBeli){
-            this.sim = sim;
-            this.nama = nama;
-            this.detikTersisa = jumlahSeconds;
-            this.isAksiPasif = isAksiPasif;
-            this.indexBeli = indexBeli;
-        }
+    //     public Aksi(Sim sim, String nama, int jumlahSeconds, boolean isAksiPasif, int indexBeli){
+    //         this.sim = sim;
+    //         this.nama = nama;
+    //         this.detikTersisa = jumlahSeconds;
+    //         this.isAksiPasif = isAksiPasif;
+    //         this.indexBeli = indexBeli;
+    //     }
 
-        public Aksi(Sim sim, String nama, int jumlahSeconds, boolean isAksiPasif, Room roomUpgrade){
-            this.sim = sim;
-            this.nama = nama;
-            this.detikTersisa = jumlahSeconds;
-            this.isAksiPasif = isAksiPasif;
-            this.roomUpgrade = roomUpgrade;
-        }
+    //     public Aksi(Sim sim, String nama, int jumlahSeconds, boolean isAksiPasif, Room roomUpgrade){
+    //         this.sim = sim;
+    //         this.nama = nama;
+    //         this.detikTersisa = jumlahSeconds;
+    //         this.isAksiPasif = isAksiPasif;
+    //         this.roomUpgrade = roomUpgrade;
+    //     }
 
-        public String getNama(){
-            return nama;
-        }
-        public boolean getIsAksiPasif(){
-            return isAksiPasif;
-        }
-        public int getDetikTersisa(){
-            return detikTersisa;
-        }
-        public void setDetikTersisa(int detikTersisa){
-            this.detikTersisa = detikTersisa;
-        }
-        public void decDetikTersisa(){
-            this.detikTersisa -= 1;
-        }
-        public int getIndexBeli(){
-            return indexBeli;
-        }
-        public Room getRoomUpgrade(){
-            return roomUpgrade;
-        }
-        public static List<Aksi> getDaftarAksiAktif(){
-            List<Aksi> listAksiAktif = new ArrayList<Aksi>();
-            listAksiAktif.add(new Aksi("KERJA", false));
-            listAksiAktif.add(new Aksi("OLAHRAGA", false));
-            listAksiAktif.add(new Aksi ("TIDUR", true));
-            listAksiAktif.add(new Aksi ("MAKAN", true));
-            listAksiAktif.add(new Aksi ("MEMASAK", true));
-            listAksiAktif.add(new Aksi ("BERKUNJUNG", false));
-            listAksiAktif.add(new Aksi ("BUANG AIR", true));
-            listAksiAktif.add(new Aksi ("YOGA", false));
-            listAksiAktif.add(new Aksi ("IBADAH", false));
-            listAksiAktif.add(new Aksi ("MENGGAMBAR", true));
-            listAksiAktif.add(new Aksi ("MAIN MUSIK", true));
-            listAksiAktif.add(new Aksi ("MANDI",true));
-            listAksiAktif.add(new Aksi ("MEMBERSIHKAN RUMAH", true));
-            listAksiAktif.add(new Aksi ("PROYEKAN", true));
-            return listAksiAktif;
-        }
-        public static List<Aksi> getDaftarAksiNonWaktu(){
-            List<Aksi> listAksiNonWaktu = new ArrayList<Aksi>();
-            listAksiNonWaktu.add(new Aksi("BERPINDAH RUANGAN", false));
-            listAksiNonWaktu.add(new Aksi("MELIHAT INVENTORY", false));
-            listAksiNonWaktu.add(new Aksi ("MEMASANG BARANG", false));
-            listAksiNonWaktu.add(new Aksi ("MELIHAT WAKTU", true));
-            return listAksiNonWaktu;
-        }
-        public static List<Aksi> getDaftarAksiAFK(){
-            List<Aksi> listAksiAFK = new ArrayList<Aksi>();
-            listAksiAFK.add(new Aksi("UPGRADE RUMAH", false));
-            listAksiAFK.add(new Aksi("BELI BARANG", false));
-            return listAksiAFK;
-        }
-    }
+    //     public String getNama(){
+    //         return nama;
+    //     }
+    //     public boolean getIsAksiPasif(){
+    //         return isAksiPasif;
+    //     }
+    //     public int getDetikTersisa(){
+    //         return detikTersisa;
+    //     }
+    //     public void setDetikTersisa(int detikTersisa){
+    //         this.detikTersisa = detikTersisa;
+    //     }
+    //     public void decDetikTersisa(){
+    //         this.detikTersisa -= 1;
+    //     }
+    //     public int getIndexBeli(){
+    //         return indexBeli;
+    //     }
+    //     public Room getRoomUpgrade(){
+    //         return roomUpgrade;
+    //     }
+    //     public static List<Aksi> getDaftarAksiAktif(){
+    //         List<Aksi> listAksiAktif = new ArrayList<Aksi>();
+    //         listAksiAktif.add(new Aksi("KERJA", false));
+    //         listAksiAktif.add(new Aksi("OLAHRAGA", false));
+    //         listAksiAktif.add(new Aksi ("TIDUR", true));
+    //         listAksiAktif.add(new Aksi ("MAKAN", true));
+    //         listAksiAktif.add(new Aksi ("MEMASAK", true));
+    //         listAksiAktif.add(new Aksi ("BERKUNJUNG", false));
+    //         listAksiAktif.add(new Aksi ("BUANG AIR", true));
+    //         listAksiAktif.add(new Aksi ("YOGA", false));
+    //         listAksiAktif.add(new Aksi ("IBADAH", false));
+    //         listAksiAktif.add(new Aksi ("MENGGAMBAR", true));
+    //         listAksiAktif.add(new Aksi ("MAIN MUSIK", true));
+    //         listAksiAktif.add(new Aksi ("MANDI",true));
+    //         listAksiAktif.add(new Aksi ("MEMBERSIHKAN RUMAH", true));
+    //         listAksiAktif.add(new Aksi ("PROYEKAN", true));
+    //         return listAksiAktif;
+    //     }
+    //     public static List<Aksi> getDaftarAksiNonWaktu(){
+    //         List<Aksi> listAksiNonWaktu = new ArrayList<Aksi>();
+    //         listAksiNonWaktu.add(new Aksi("BERPINDAH RUANGAN", false));
+    //         listAksiNonWaktu.add(new Aksi("MELIHAT INVENTORY", false));
+    //         listAksiNonWaktu.add(new Aksi ("MEMASANG BARANG", false));
+    //         listAksiNonWaktu.add(new Aksi ("MELIHAT WAKTU", true));
+    //         return listAksiNonWaktu;
+    //     }
+    //     public static List<Aksi> getDaftarAksiAFK(){
+    //         List<Aksi> listAksiAFK = new ArrayList<Aksi>();
+    //         listAksiAFK.add(new Aksi("UPGRADE RUMAH", false));
+    //         listAksiAFK.add(new Aksi("BELI BARANG", false));
+    //         return listAksiAFK;
+    //     }
+    // }
 
 
      /* Sim Attributes */
