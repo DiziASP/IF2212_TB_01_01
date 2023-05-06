91e00aa2013cf799b01f288843ffbb3df3574b89
nadiraaraa <nadiraaraa@gmail.com>
2023-05-06 19:13:53 +0700

    kunjungi rumah donee!

--- app/src/main/java/if2212_tb_01_01/utils/KeyHandler.java
+++ app/src/main/java/if2212_tb_01_01/utils/KeyHandler.java
@@ -33,6 +33,7 @@ public class KeyHandler implements KeyListener {
     String input = "";
     int in1=0, in2=0;
     int pointer = 0;
+    Point p;
 
     public KeyHandler(GamePanel gp) {
         this.gp = gp;
@@ -428,6 +429,7 @@ public class KeyHandler implements KeyListener {
                             }
                             break;
                         case "kunjungi rumah":
+                            input = gp.getOpsiAksi(arrowNum);
                             gp.setSubState(11);
                             if (arrowNum == gp.getIndexActiveSim()) {
                                 arrowNum++;
@@ -569,11 +571,11 @@ public class KeyHandler implements KeyListener {
 
                         // this sim
                         Sim s = gp.getWorldClock().getWorld().getSim(arrowNum);
+                        p = s.getPosisiRumah();
                         gp.setRoom(s.getRoomAwal());
-                        gp.getRoom().setIsBuilded(true);
                         gp.showNotification("berkunjung ke rumah "+s.getNamaLengkap());
-                        gp.setSubState(16);
-
+                        gp.getSim().berkunjung(p);
+                        gp.setSubState(14);
 
 //                } else if (gp.getSubState()==12){
 //                    //tambah ruang upgrade rumah
@@ -696,7 +698,10 @@ public class KeyHandler implements KeyListener {
                     //aksi counter
                     if (gp.getActionCounter()==0){
                         gp.soundManager.stop();
-                        gp.setSubState(0);
+                        if (input == "kunjungi rumah"){
+                            gp.setSubState(16);
+                        }
+                        else {gp.setSubState(0);}
                     }
                 } else if (gp.getSubState()==15){
                     //aksi berhasil
@@ -708,7 +713,9 @@ public class KeyHandler implements KeyListener {
                             gp.setGs(7);
                             break;
                         case "pulang":
-                            gp.setSubState(0);
+                            input = gp.getOpsiAksi(arrowNum);
+                            gp.getSim().berkunjung(p);
+                            gp.setSubState(14);
                             gp.setRoom(gp.getSim().getRoomAwal());
                             break; 
                         case "pindah ruangan":
